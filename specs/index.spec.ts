b205a6e32e9c078d9b8c4ee5758dd27020b1a221
kurt corbett <kurtcorbett@hotmail.com>
2018-04-26 01:12:36 -0400

    support effect iterable tuple test syntax

---
+++ specs/index.spec.ts
@@ -0,0 +1,331 @@
+import { expect } from 'chai';
+import { isAsyncIterable, isIterable } from 'iterall'
+import { Effect, testFn, EffectIterable, EffectIteratorResult, executeEffects, returnStub } from '../src'
+
+function add(a, b): number {
+  return a + b
+}
+
+class TestClass {
+  @Effect
+  public static async add(arg1: number, arg2: number): Promise<number> {
+    return add(arg1, arg2)
+  }
+}
+
+
+describe('EffectIterable', () => {
+
+  context('when testMode=false', () => {
+    let effectIterator;
+    let expected;
+    let result;
+    const args = [1,2];
+
+    beforeEach(async () => {
+      effectIterator = new EffectIterable(add).withArgs(...args);
+
+      result = await effectIterator.next()
+    })
+
+
+    it('should return an iterator result', async () => {
+      expect(result).to.have.deep.property('done')
+      expect(result).to.have.deep.property('value')
+    })
+
+    it('should return an iterator for a value', () => {
+      expect(result.value).to.have.deep.property('next')
+      expect(result.value).to.have.deep.property('return')
+      expect(result.value).to.have.deep.property('throw')
+    })
+  })
+
+  context('when testMode=true', () => {
+    let effectIteratorResult;
+    let expected;
+    const args = [1,2]
+
+    beforeEach(() => {
+      effectIteratorResult = new EffectIterable(add).withArgs(...args).returnStub(5);
+
+      expected = {
+        done: true,
+        value: new EffectIteratorResult({
+          args,
+          methodName: 'add',
+          value: 5,
+        })
+      }
+    })
+
+    it('should return a value when next is called', async () => {
+      expect(await effectIteratorResult.next(5)).to.deep.eq(expected)
+    })
+  })
+})
+
+describe('@Effect', () => {
+
+  context('when iterating over a @Effect decorated method with testMode=true', () => {
+    const args = [1,2]
+    let effectIterator;
+    let result;
+    let expected;
+
+    beforeEach(async function () {
+      effectIterator = TestClass.add(1,2)
+
+      result = await effectIterator.next()
+    })
+
+    it('should return an iterator result', async () => {
+      expect(result).to.have.deep.property('done')
+      expect(result).to.have.deep.property('value')
+    })
+
+    it('should return an iterator for a value', () => {
+      expect(result.value).to.have.deep.property('next')
+      expect(result.value).to.have.deep.property('return')
+      expect(result.value).to.have.deep.property('throw')
+    })
+  })
+})
+
+async function * coordinator(num: number) {
+  const effect1Result = yield await new EffectIterable(add)
+  .withArgs(num, num)
+
+  const result = yield await new EffectIterable(add)
+  .withArgs(effect1Result, num)
+
+  return result
+}
+
+describe('executeEffects', () => {
+  context('flat effect', () => {
+    it('should return a result', async () => {
+      expect(await executeEffects(coordinator(1))).to.eq(3)
+    })
+  })
+
+  context('nested effects', () => {
+    async function * nestedCoordinator(num: number) {
+      return yield await new EffectIterable(coordinator).withArgs(num)
+    }
+
+    it('should return a result', async () => {
+      expect(await executeEffects(nestedCoordinator(1))).to.eq(3)
+    })
+  })
+
+  context('nested with flat', () => {
+    async function * nestedCoordinator(num: number) {
+      const res1 = yield await new EffectIterable(coordinator).withArgs(num)
+      const result = yield await new EffectIterable(add).withArgs(res1, num)
+      return result
+    }
+
+    it('should return a result', async () => {
+      expect(await executeEffects(nestedCoordinator(1))).to.eq(4)
+    })
+  })
+
+  context('nested async generators', () => {
+    let result;
+
+    beforeEach(async () => {
+      async function * parentGen(num) {
+        const result = yield await childGen(num)
+        return result;
+      }
+
+      async function * childGen(num) {
+        return num + 1;
+      }
+
+      result = await executeEffects(parentGen(1));
+    })
+
+    it('should return the correct result', () => {
+      expect(result).to.eq(2)
+    })
+  })
+
+  context('deeply nested async generators', () => {
+    let result;
+
+    beforeEach(async () => {
+      async function * greatGrandparentGen(num) {
+        const result = yield await grandparentGen(num)
+        return result + 1;
+      }
+
+      async function * grandparentGen(num) {
+        const result = yield await parentGen(num)
+        return result + 1;
+      }
+
+      async function * parentGen(num) {
+        const result = yield await childGen(num)
+        return result + 1;
+      }
+
+      async function * childGen(num) {
+        return num + 1;
+      }
+
+      result = await executeEffects(greatGrandparentGen(1));
+    })
+
+    it('should return the correct result', () => {
+      expect(result).to.eq(5)
+    })
+  })
+
+  context('async generator', () => {
+    let result;
+
+    beforeEach(async () => {
+      async function * childGen(num) {
+        return num + 1;
+      }
+
+      result = await executeEffects(childGen(1));
+    })
+
+    it('should return the correct result', () => {
+      expect(result).to.eq(2)
+    })
+  })
+
+  context('single EI', () => {
+    function add(a, b) {
+      return a + b;
+    }
+
+    async function * singleEI(num) {
+      return yield new EffectIterable(add).withArgs(num, num)
+    }
+
+    it('should return 2', async () => {
+      expect(await executeEffects(singleEI(1))).to.eq(2)
+    })
+  })
+
+  context('multiple EI', () => {
+    function add(a, b) {
+      return a + b;
+    }
+
+    async function * multipleEI(num) {
+      const res1 = yield await new EffectIterable(add).withArgs(num, num)
+
+      return yield new EffectIterable(add).withArgs(res1, res1)
+    }
+
+    it('should return 2', async () => {
+      expect(await executeEffects(multipleEI(1))).to.eq(4)
+    })
+  })
+
+})
+
+describe('testFn', () => {
+  context('when testing a single decorated @EffectIterable with Object syntax', () => {
+    let addCordinatorFnTest;
+    let result;
+
+    beforeEach(async () => {
+      class TestClass {
+        @Effect
+        public static async add(arg1: number, arg2: number): Promise<number> {
+          return arg1 + arg2
+        }
+      }
+
+      async function * addCoordinator(num) {
+        const result = yield await TestClass.add(1,1);
+        return result + num;
+      }
+
+      addCordinatorFnTest = testFn(addCoordinator)
+
+      result = await addCordinatorFnTest({
+        args: [ 1 ],
+        effects: [
+          [ TestClass.add(1,1), returnStub(5) ]
+        ],
+        output: 6
+      })
+    })
+
+    it('result should return true', () => {
+      expect(result).to.eq(true)
+    })
+  })
+
+  context('when testing multiple ei with Object syntax', () => {
+    let addManyTest;
+    let result;
+
+    beforeEach(async () => {
+      async function * addMany(num) {
+        const res1 = yield await new EffectIterable(add).withArgs(num,num);
+        const res2 = yield await new EffectIterable(add).withArgs(res1, res1);
+
+        return res2 + num;
+      }
+
+      addManyTest = testFn(addMany)
+
+      result = await addManyTest({
+        args: [ 1 ],
+        effects: [
+          new EffectIterable(add).withArgs(1,1).returnStub(2),
+          new EffectIterable(add).withArgs(2,2).returnStub(4),
+        ],
+        output: 5
+      }).catch((err) => result = err)
+    })
+
+    it('result should return true', () => {
+      expect(result).to.eq(true)
+    })
+  })
+
+  async function * effectPlusPure(num) {
+    const eiResult1 = yield await new EffectIterable(add).withArgs(num,num);
+    const eiResult2 = yield await new EffectIterable(add).withArgs(eiResult1, eiResult1);
+
+    return eiResult2 + num;
+  }
+
+  async function * effectPlusPureCoordinator(num) {
+    const coordinatorResult = yield await effectPlusPure(num)
+
+    return coordinatorResult + num
+  }
+
+  context('when testing nested ei with Object syntax', () => {
+    let effectPlusPureCoordinatorTest;
+    let result;
+
+    beforeEach(async () => {
+
+      effectPlusPureCoordinatorTest = testFn(effectPlusPureCoordinator)
+
+      result = await effectPlusPureCoordinatorTest({
+        args: [ 1 ],
+        effects: [
+          new EffectIterable(effectPlusPure).withArgs(1).returnStub(5),
+        ],
+        output: 6
+      })
+    })
+
+    it('result should return true', () => {
+      expect(result).to.eq(true)
+    })
+  })
+})
---
+++ src/index.ts
@@ -0,0 +1,189 @@
+(<any>Symbol).asyncIterator = Symbol.asyncIterator || Symbol.for("Symbol.asyncIterator")
+import { $$iterator, $$asyncIterator, forAwaitEach, getIterator, isAsyncIterable } from 'iterall'
+import { deepEqual } from 'assert'
+import { expect } from 'chai'
+
+export function Effect(target: Function, key: string, descriptor: any) {
+  // save a reference to the original method
+  // this way we keep the values currently in the
+  // descriptor and don't overwrite what another
+  // decorator might have done to the descriptor.
+  var originalMethod = descriptor.value
+
+  descriptor.value = function(...args: any[]) {
+    return new EffectIterable(originalMethod, {
+      methodName: key,
+      isDecorated: true,
+    }).withArgs(...args)
+  }
+
+  return descriptor
+}
+
+export class EffectIterable {
+  protected isDecorated: string
+  private args: any[]
+  private stub: any
+  testMode: boolean
+  methodName: string
+
+  constructor(
+    public fn: (...args: any[]) => any,
+    opts?: IOIteratorOptions,
+  ){
+    this.methodName = opts && opts.methodName || fn.name
+  }
+
+  withArgs(...args) {
+    this.args = args
+    return this
+  }
+
+  public returnStub(value?: any) {
+    this.testMode = true
+    this.stub = value
+    return this
+  }
+
+  [$$asyncIterator]() {}
+
+  async next(): Promise<IteratorResult<any>> {
+
+    if(this.testMode) {
+      return {
+        done: true,
+        value: new EffectIteratorResult({
+          methodName: this.methodName,
+          args: this.args,
+          value: this.stub
+        })
+      }
+    } else {
+      const klass = this;
+      async function * genWrapper() {
+        return klass.fn(...klass.args)
+      }
+
+      return {
+        done: true,
+        value: genWrapper()
+      }
+    }
+  }
+
+  async return(value?: any): Promise<IteratorResult<any>> {
+    return {
+      done: true,
+      value,
+    }
+  }
+}
+
+
+export async function executeEffects(parentGen, child = undefined) {
+  if (isAsyncIterable(parentGen)) {
+    const parentResult = isAsyncIterable(child)
+      ? await parentGen.next(await executeEffects(child))
+      : await parentGen.next()
+
+
+    if(parentResult.done) {
+      if (isAsyncIterable(parentResult.value)) {
+        return executeEffects(parentResult.value)
+      }
+      return parentResult.value
+    } else {
+      return executeEffects(parentGen, parentResult.value)
+    }
+  } else {
+    throw new TypeError(`#executeEffects must be called with an iterable. Did you forget to call your generator function?`)
+  }
+
+}
+
+interface ISequenceMap {
+  args: any[],
+  effects: EffectIterable[],
+  output: any
+}
+
+export function returnStub(value?: any) {
+  return value;
+}
+
+export function testFn(fnInTest: (...args) => AsyncIterable<any>) {
+  return async function tester({ args, effects, output }: ISequenceMap) {
+
+
+    const coordinator = fnInTest(...args)
+    const expectedIteration = getIterator(effects)
+
+    const finalResult = await testCoordinator(coordinator, expectedIteration)
+    deepEqual(finalResult, output)
+    return true
+  }
+}
+
+async function testCoordinator(coordinator, expected, value = undefined) {
+  const yielded = await coordinator.next(value)
+
+  if(yielded.done === false) {
+    const isYieldedAGenFn = yielded.value instanceof EffectIterable === false && isAsyncIterable(yielded.value)
+    const expectedEI = await expected.next().value
+    const ei = yielded.value
+
+    if(isYieldedAGenFn) {
+      const genResult = await yielded.value.return(expectedEI.stub)
+      return testCoordinator(coordinator, expected, genResult.value)
+    }
+
+    if(ei instanceof EffectIterable) {
+      const isEffectDecoratorEI = Array.isArray(expectedEI) && expectedEI[0] instanceof EffectIterable
+      let eiResult;
+
+      if (isEffectDecoratorEI) {
+        await testEffect(expectedEI[0], ei)
+        eiResult = await ei.return(expectedEI[1])
+      } else {
+        await testEffect(expectedEI, ei)
+        eiResult = await ei.return(expectedEI.stub)
+      }
+      return testCoordinator(coordinator, expected, eiResult.value)
+    } else {
+      throw new Error('Not an EffectIterable')
+    }
+  } else {
+    return yielded.value
+  }
+}
+
+async function testEffect(expected, ei) {
+  deepEqual(expected.fn, ei.fn)
+  deepEqual(expected.args, ei.args)
+}
+
+export class EffectIteratorResult {
+  methodName: string
+  args: any[]
+  value: any
+
+  constructor(
+    data: IEffectIteratorResult
+  ) {
+    Object.keys(data).forEach((key) => {
+      this[key] = data[key]
+    })
+  }
+}
+
+interface IEffectIteratorResult {
+  methodName: string
+  args: any[]
+  value: any
+}
+
+
+interface IOIteratorOptions {
+  methodName?: string
+  isDecorated?: boolean
+}
